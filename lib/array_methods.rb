1838cb0ca2b5d9130cb7408da98f4b6f8e0cc2c3
Neeraj Khosla <n.khosla91@gmail.com>
2019-08-13 17:27:01 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -13,10 +13,10 @@ def find_max_value(array)
   newarr = [array[0]]
   while array[counter] do
     if (array[counter] >= newarr[counter])
-      newarr.unshift(array[counter])
+      newarr.push(array[counter])
       counter += 1
     else 
-      newarr.p(array[counter])
+      newarr.unshift(array[counter])
       counter += 1
     end
     return newarr[-1]
