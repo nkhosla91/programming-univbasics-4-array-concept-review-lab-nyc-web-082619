8cd825b4ae2e7530b04f0af8689258153d57db2a
Neeraj Khosla <n.khosla91@gmail.com>
2019-08-13 17:03:33 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,4 +1,12 @@
-def find_element_index(array, value_to_find)
+def find_elemendef find_element_index(array, value_to_find)
+  counter = 0
+  while array[counter] do
+    if (array[counter] == value_to_find)
+      p counter
+    end
+    counter =+ 1
+  end
+endt_index(array, value_to_find)
 
 end
 
